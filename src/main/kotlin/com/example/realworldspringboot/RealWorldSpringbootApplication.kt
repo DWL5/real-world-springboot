a8ea2bd7e3d9fa6ece9405e232933d9d7f6d7f56
DWL5 <ssop6403@gmail.com>
2024-02-03 17:53:49 +0900

    feat : create project

---
+++ src/main/kotlin/com/example/realworldspringboot/RealWorldSpringbootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.realworldspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealWorldSpringbootApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealWorldSpringbootApplication>(*args)
+}
